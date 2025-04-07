7fcc258907ee6dd60f877f650a2173e5abac5903
Hizzaky <37980872+Hizzaky@users.noreply.github.com>
2025-04-07 14:29:01 +0900

    46.254.93.4

--- _worker.js
+++ _worker.js
@@ -5,7 +5,7 @@ import { connect } from 'cloudflare:sockets';
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = 'b2ac46ed-7cfb-4a49-a271-e08a9802d52c';
 
-const พร็อกซีไอพีs = ['46.254.93.111'];
+const พร็อกซีไอพีs = ['46.254.93.4'];
 // 188.166.199.61
 // 157.245.158.169
 // 128.199.218.226 download
@@ -18,6 +18,7 @@ const พร็อกซีไอพีs = ['46.254.93.111'];
 // 152.42.242.148
 // 8.219.104.189
 // 144.126.204.130
+// 46.254.93.111  game
 
 // if you want to use ipv6 or single พร็อกซีไอพี, please add comment at this line and remove comment at the next line
 let พร็อกซีไอพี = พร็อกซีไอพีs[Math.floor(Math.random() * พร็อกซีไอพีs.length)];
